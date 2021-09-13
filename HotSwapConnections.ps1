e402a09545f044afe5cdb9ea0a342f97c79e9bf8
PowerBISteve <68716422+PowerBISteve@users.noreply.github.com>
2021-09-13 16:19:03 -0500

    Update HotSwapConnections.ps1
    Added warning label
    resized canvas to fit all text

--- HotSwapConnections.ps1
+++ HotSwapConnections.ps1
@@ -257,6 +257,8 @@ $menuStrip1 = New-Object System.Windows.Forms.MenuStrip
 $tabPage5 = New-Object System.Windows.Forms.TabPage
 $textBox1 = New-Object System.Windows.Forms.TextBox
 $label6 = New-Object System.Windows.Forms.Label
+$warnlabel = New-Object System.Windows.Forms.Label
+$warnlabel1 = New-Object System.Windows.Forms.Label
 $label5 = New-Object System.Windows.Forms.Label
 $tabPage3 = New-Object System.Windows.Forms.TabPage
 $label2 = New-Object System.Windows.Forms.Label
@@ -289,14 +291,14 @@ $tabPage5.Controls.Add($label6)
 $tabPage5.Controls.Add($textBox1)
 $tabPage5.Location = New-Object System.Drawing.Point(4, 22)
 $tabPage5.Name = "tabPage5"
-$tabPage5.Size = New-Object System.Drawing.Size(266, 163)
+$tabPage5.Size = New-Object System.Drawing.Size(266, 193)
 $tabPage5.TabIndex = 4
 $tabPage5.Text = "Settings"
 $tabPage5.UseVisualStyleBackColor = $true
 #
 # textBox1
 #
-$textBox1.Location = New-Object System.Drawing.Point(50, 75)
+$textBox1.Location = New-Object System.Drawing.Point(60, 75)
 $textBox1.Name = "textBox1"
 $textBox1.Size = New-Object System.Drawing.Size(113, 20)
 $textBox1.TabIndex = 4
@@ -320,7 +322,7 @@ $tabPage3.Controls.Add($label2)
 $tabPage3.Controls.Add($label5)
 $tabPage3.Location = New-Object System.Drawing.Point(4, 22)
 $tabPage3.Name = "tabPage3"
-$tabPage3.Size = New-Object System.Drawing.Size(266, 163)
+$tabPage3.Size = New-Object System.Drawing.Size(266, 193)
 $tabPage3.TabIndex = 2
 $tabPage3.Text = "Help"
 $tabPage3.UseVisualStyleBackColor = $true
@@ -345,34 +347,54 @@ $label3.Text = "Remove: Clear all connections and/or data models from selected f
 # linkLabel1
 #
 $linkLabel1.AutoSize = $true
-$linkLabel1.Location = New-Object System.Drawing.Point(3, 120)
+$linkLabel1.Location = New-Object System.Drawing.Point(1, 120)
 $linkLabel1.Name = "linkLabel1"
 $linkLabel1.Size = New-Object System.Drawing.Size(237, 13)
 $linkLabel1.TabIndex = 4
 $linkLabel1.TabStop = $true
 $linkLabel1.Font = New-Object System.Drawing.Font("Microsoft Sans Serif", 8)
-$linkLabel1.Text = "Click here for the documentation on PowerBI.tips"
+$linkLabel1.Text = "Click here for documentation on PowerBI.tips"
 $LinkLabel1.add_Click({ Open-HelpPage })
 #
 # label5
 #
 $label5.AutoSize = $true
-$label5.Location = New-Object System.Drawing.Point(43, 145)
+$label5.Location = New-Object System.Drawing.Point(1, 140)
 $label5.Name = "label5"
 $label5.Size = New-Object System.Drawing.Size(237, 13)
 $label5.Font = New-Object System.Drawing.Font("Microsoft Sans Serif", 7.5)
 $label5.TabIndex = 5
 $label5.Text = "Hot Swap Report Connections —— Version " + $version
 #
+# warnlabel
+#
+$warnlabel.Location = New-Object System.Drawing.Point(3, 150)
+$warnlabel.Name = "warnlabel"
+$warnlabel.Size = New-Object System.Drawing.Size(255, 50)
+$warnlabel.TabIndex = 0
+$warnlabel.Text = "THIS MODIFIES FILES IN A WAY UNSOPPORTED BY MICROSOFT. ALWAYS BACK UP ALL FILES."
+
+
+
+$warnlabel1.Location = New-Object System.Drawing.Point(3, 150)
+$warnlabel1.Name = "warnlabel1"
+$warnlabel1.Size = New-Object System.Drawing.Size(250, 50)
+$warnlabel1.TabIndex = 9
+$warnlabel1.Text = "THIS MODIFIES FILES IN A WAY UNSOPPORTED BY MICROSOFT. ALWAYS BACK UP ALL FILES."
+#
 # tabPage2
 #
+
+
+
 $tabPage2.Controls.Add($linkLabel3)
 $tabPage2.Controls.Add($reml_cop)
 $tabPage2.Controls.Add($reml_ovr)
+$tabPage2.Controls.Add($warnlabel1)
 $tabPage2.Location = New-Object System.Drawing.Point(4, 22)
 $tabPage2.Name = "tabPage2"
 $tabPage2.Padding = New-Object System.Windows.Forms.Padding(3)
-$tabPage2.Size = New-Object System.Drawing.Size(266, 163)
+$tabPage2.Size = New-Object System.Drawing.Size(266, 193)
 $tabPage2.TabIndex = 1
 $tabPage2.Text = "Remove"
 $tabPage2.UseVisualStyleBackColor = $true
@@ -399,13 +421,16 @@ $reml_cop.UseVisualStyleBackColor = $false
 #
 # tabPage1
 #
+
+
 $tabPage1.Controls.Add($linkLabel2)
 $tabPage1.Controls.Add($con_cop)
 $tabPage1.Controls.Add($con_ovr)
+$tabPage1.Controls.Add($warnlabel)
 $tabPage1.Location = New-Object System.Drawing.Point(4, 22)
 $tabPage1.Name = "tabPage1"
 $tabPage1.Padding = New-Object System.Windows.Forms.Padding(3)
-$tabPage1.Size = New-Object System.Drawing.Size(266, 163)
+$tabPage1.Size = New-Object System.Drawing.Size(266, 193) 
 $tabPage1.TabIndex = 0
 $tabPage1.Text = "Connect"
 $tabPage1.UseVisualStyleBackColor = $true
@@ -438,7 +463,7 @@ $tabControl1.Controls.Add($tabPage3)
 $tabControl1.Location = New-Object System.Drawing.Point(0, 0)
 $tabControl1.Name = "tabControl1"
 $tabControl1.SelectedIndex = 0
-$tabControl1.Size = New-Object System.Drawing.Size(274, 185)
+$tabControl1.Size = New-Object System.Drawing.Size(274, 239)
 $tabControl1.TabIndex = 4
 #
 # label1
@@ -483,7 +508,7 @@ $LinkLabel3.add_Click({ Open-HelpPage })
 #
 # HotSwap
 #
-$HotSwap.ClientSize = New-Object System.Drawing.Size(275, 186)
+$HotSwap.ClientSize = New-Object System.Drawing.Size(275, 240)
 $HotSwap.Controls.Add($tabControl1)
 $HotSwap.Controls.Add($menuStrip1)
 $iconBase64 = 'iVBORw0KGgoAAAANSUhEUgAAAPsAAAD6CAYAAABnLjEDAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAALEgAACxIB0t1+/AAAFdFJREFUeF7tncuR4zgCRMeEtmC3TWgTxoS+7q1MaBPGhDKhTKjDXjeiTajr3sqAObQHvZlaUUFBSYH4kgTz8EJVEPGhgEcCIEj+8fv3b2PMCZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQgcaY8ZCBxpjxkIHGmPGQga34+z///PPKX8aYf/4A9OGL8qU2MrAG3AHwHbyBD/DbGLPIJ6Ar35VPNZCBJaCwPFKx0GqHjDFxfoFX8FU5losMzAEFewE8OqnCG2Py4ImzivQyMAUUhGdyd9ONaQvH+EVjexm4BmYM2NVQBTPG1Icn1W/KxzXIwBjMELjLbsw2vCgvY8jAZzAjwAkEVQhjTB/elJ/PkIFLIAOKrjI2xvQnSXgZqEDCFt2Y/bFaeBkYggQtujH75S/lbYgMnIOEOBlXa4zO2cR3MC0X5EFkWkJrzBngElm2/dorS6Mr72TgBBLg5bXSWffLEkDQZf2vMUeCXlz9KF11yhPy08U3MnACkXkWVgmvoXgRgDFngr5cvcntSf9U6U7IQIKI7HKoBGP8BFXX9BpzJugPyD3RLl6Dl4EEkXK6768qLWNMOvCJ43vl2TPYK5A96ocAgo1zZt+zVvUYY5ahV4Fna5Cz8w8BBBunntV9RjemEfArVXh5dr/7h2AjzgyqBJZ4OilgjCkHnnHiTvm3xENP++4fgo1SJgai0/3GmDrANU5+Kw8VH2H8u3+wAaf+VcQlVq3cMcaUA9+4wE15uMTdiThMLGVssDjrZ4xpA5xLWXzzYx63JKHsSbl//fu/L+CvnfGnKqsxewLe8Rq88lFxN58WJpQyC589VodYP8HvneEhiTkEcG/1mvp5vDARGUHwMPhPAWJZdmMygX8pi21uPdZ5AinLY4uuq0Msy25MJvAvpSt/G7fPE0iRvWh8C7EsuzEFwMG1Q+5bu55HTrloXzQLD7EsuzEFwMG162GKZP+c4uQCsSy7MQXAw7W+FslevDwWYll2YwqAh2sn6YpkT36EbQjEsuzGFAAP186xFcleLAXEsuzGFAAPLXsBlt0cBnho2Quw7OYwwMPTyc50aqVl2c1hgIenkZ03rtyu9ePvr+D9+l0ult0cBnh4Ctnvbtubg+/egm1TsOzmMMDD4WV/uqAH338Bv2bbp2DZzWGAh8PLHs0b2+Se3S27OQzwcHjZozfgYBuO51XcGJbdHAZ4OLzsi+P1CWzDybpX8HmNs5ai/UL8knmIVvjJv4MCD4eXPanxYvtvYK34lt0cBng4vOxk8dW0+I7jdU7Q8fNuO/4/+16la9nNYYCHp5Cdsj4IjzA1McdteWb/FmyrxP8AfBBm1r36iGfZTTfg4Slkn6Ccl7Lwcxa+BLvya8XnAp2kd9dhe8tuugEPTyX7hWt6f4bhEdaKz0/+vzh0mMA2lt10Ax6eT3bA2XdOxKnv1lBFfIRbdtMNeHhK2S/X3kV4Dtni42/LvkP+/s8/XsAb+ARov3d8AH4X7bntDXh4Stkv5cHnmstrz2CZOA8w/b8kPifx5jfdTOKX5t+C08oOgX+AXwBtdhXctrht9wIenlL2yzPs8Vma5nTQ4LDgBwjF5yTg7S04+Jvr8EPx98bpZIewX8DPq8A5sAdwd4DfI/DwlLJfGjQ+c5fJTjzsF8KU+PybYUcQ/1SyQ1KKzq452mkRPMvvumsPD08p++VOOHxSQPX9Wp7uF74/ovinkR1y1hJ9zm7P8PDwlLIjyUuaqZffQlbvF7ZtIT7H/tyWaTMeJwXn6edwJtlfA1FrwDP8Ll9LDg+PIXst0JgnwXnpjZLw76TFMKVc8+ZE3jRTT1LF5/ayUSGck3/h9ms5hewQEg1fylqDd5Xn1sDD08k+XV+fX37bpHzId6m3siT+vGeweOvuddvcmf6zyP4eCFqboncbtgAenkt2cm3UlzLhk/JsciSmWNeyPIPl49n97vl583QU2IY9B5VejOFlh4hfAzFbUPxilNrAw1PKzrPepTLYuLdq4Ne8lXBLsDt/J/4S2CZ3PuIMsvN6OtplW8J8twYenlL2m+D4vFx+m3/fi2s5lHAxomcNbMPJOhU3xhlkL7mmnsKuuvLw8JSyXybHrn9zHMxG3n0GFXnmyr7m6Tvuxi8ACVNWyZWwq3YPD08p++1sjs+pu9v9KIw8c7rxa8brnKCbz/SnMLTsEJDX1tEmu3BZqbkX4OEpZb8JDigG/+56+Y0gz7Wyc5LudjDC3zxYcd7h4QyPMO7PNGOfw+iyo8FLMVuwq98SHp5S9un6+mV54/Xv7mVEnjHZL4tmZttzci68pMY0eNAiHJLkntEnLHs9LPseuDbsS7nYwEH3y2/XfEPZJnj2vswj4JMir+0FlGLZ62HZ9wAaNbu60+U3joU/wm1aQ7FAKBtXv13G5fy8/h9u0xLLXg/LvgfYqKeGjc9NLr9dy3CTDFzG5fjkuJtlKu2S52DZ62HZ9wAa9UWm698cC7OhR2e6a4B8uGR3Ep1j8Ni4vCeWvR6WvSZonJM4qUxC8Sw6zc6za6+2TWahrMxr6pbzrB2Oy0tm0Wth2eth2WuCxjmJmgvjT5ffqiHKOe+Wh+PyNbez9sKy18Oy1wSNs1T2Sxcan1XHx7PyzbvlPOuH4/KHuBtj2eth2WuCxlkq+6Vs+KSI6vtcWK4pTco+f6osDwBbTL6twbLXw7LXBI2zVPbL9XV8trjEdRmXB2WdzvIcn/NmFYbtid0/OLEECGjZ4wwr+6VC8Fm7S82DxzT5xnH5dJbnAaD70lzzfyCgZY8zrOxI5pJO7i2hIZR6mnzjuHx+99lt9t1sAwS07HHGlR3ML7/lwu75/GaV+Tr1VXermfZAQMseZ2jZpxly9V0MCn27+wx/s4fwMPtu9gEEtOxxhpY99/Ibu+jTuHy+Ko54XL5DIKBljzO07Jfy4XMu6zNu3XJ8zlfF3ZjKZ/YFBLTscYaW/VIp+IxdfuPlsvm4fL4q7o5pG7MvIKBlj3MK2ZfeqHJ3uYx/g2lcLpm2NfsCAlr2OMPKzi753eUw/D+fSb9dLsMn81rV1Z+nZ/YDBLTscYaUfbEy8B0XwsxvVklaYTdPy+wHCPgSCNmSoWR/DzZaYq+yP732je+nm1VSZ+kRXadptgUC4gQlxWzBULL/DDZaYo+yRx9DhW2yDyRhWq1Bw+IjktlFvUNte2bwm1j2OMPJvqoiRLxVhOm0AI2JQr+Bz2vjWoLvIOcrj06/VBe/gWWPM5zsnyq9Odhml2d2NCK+mDDnFUZ8E0qzujgC3P/Z79Eay14TiJUtJHjazcX3ua9QQnSdZiloQJxgKn19Ec/0pzzLY78te5whZec1ddnoET69xz2LML0aoPHUnEk+pfDYZ8seZ0jZCYW/m5XH/1xckzwDP2eeXg3QcL4FDakGu2qMPcA+W/Y4Unb8cHLjkD3LPkHpuWDm6cq4tYTlLAUNJzYJl0v0jbAjgf217HGGl70qYTlLQKNpuRCE4//TdOexr5Y9jmVPISxnCWg0rc7qE6c5u2NfLXscy55CWM5c0GBajNVDur/nbiuwr5Y9jmVPISxnLmgwvRrnKR6bhf3sKXuzdp8DPLTsLQjLmQsaTM7imRxO8WQd7Kdlj2PZUwjLmQsaTOkCmrW8qvxHA/tp2eNY9hTCcuYiGlArdjW+bAX207LH2a3svNect6HuClXWVNBYUDmyEbXAstfHspt1oLH0lP3yrvrRwX5a9jiWvTdoLD1lR5a6HCOB/bTscSx7b9BYLHtlsJ+WPY5l7w0ai2WvDPbTssex7L1BY7HslcF+WvY4lr03aCyWvTLYT8sex7L3Bo3FslcG+2nZ41j23qCxDCU78uBNPZTtHXAZ8Bw+OJO38ja93RbpW/Y4lr03aCxDyI60KXHKbboUv8mNOUjXssex7L1BYzm07Egz9ym4hPcEVL/PHmla9jiWvTdoLIeVHemxy17jJp43lX4uSM+yx7HsvUFjOaTsSItn9Jp361UTHmlZ9jiWvTdoLEeVnY+plnkUUOV+e6Rj2eNY9t6gsRxOdqTTSib2FIon7ZCGZY9j2XuDxnIo2ZEGXy7Z8mEbxd15pGHZ41j23qCxHE12vjxSpl2RouvwiG/Z41j23qCxHE32FmP1kKLLcYhv2eNY9t6gsRxGdsTnDLxMtzJFT9RBfMsex7L3Bo3lSLJ/D9NrRZh3Cohv2eNY9t6gsRxJ9p4SfVNlWAPiWvY4lr03aCxHkp03t8h0G/D0vfrPQFzLHsey9waN5Uiy566BzyF7kg5xLXscy94bNBbLrsluT4wbpNUSy27WgcZi2TWWPQN4aNn3ChqLZddY9gzgoWXfK2gsll1j2TOAh5Z9r6CxWHaNZc8AHlr2vYLGYtk1lj0DeGjZ9woai2XXWPYM4KFl3ytoLJZdY9kzgIeWfa+gsVh2jWXPAB5a9r2CxmLZNZY9A3ho2fcKGotl11j2DOChZd8raCyWXWPZM4CHln2voLFYdo1lzwAeWva9gsZi2TWWPQN4aNnXgIrjG06avnQwBPlZdo1lzwAeWnYFKoovI+QDGdSjkfmSQr58MPshCmtg+tf8uhDmnwLiW/ZHLPueYQWBlGefU/wm0jPdWT7NCfNPAfEt+yOWfY+gYvh01JJHIfNMX7Wbj/Qsu8ayZwAPLTsqpdYbR3mwqCY80rLsGsueATw8t+yokFqiT1QTHulYdo1lzwAedpG96MH+rUBl8N1kLd5i8q7ySwXpWHaNZc8AHp5a9pYV/13lmQLSsOway54BPHwJvFyiSPZfU5y9gIpo/bqiT5VvCkjDsmssewbwcK2vUvYfwUbP6LogJQYq4jWomBa8qLzXgviWXWPZM4CDqCPpZoiUfe0YgDRdgJIKKqLle8QnisbuiG/ZNZY9AziINi/dDLmdpOaRvwUbPeN1irc1qATOwKNMXcju0SCuZddY9kTg3/fAx2fcTsxhImpjxcc83pagEjD8kJXTgpL3kll2jWVPBP69BT4+43aCChNZOw4gX+dxt4KVEFRKS0reS2bZNZY9AXj3Baztwt+dlMOE8OPJSIpddOVRCT3fOFrSMC27xrInAO+yHQ0TShm38+iy+aw8KuEoDdOyayz7Sujb1TuUZxV360PuEiPY4DOI8Iwd/ACWXRHmnwLiW/ZH9iD7a+DfMx7Ww9z9Q7BRyvV28i1MoyeoBMsuCPNPAfEt+yObyk7PAu9iPAyz7/4h2IhdBRV5iQ+wWXcelWDZBWH+KSC+ZX9ks2Xi9Auk9LjJwwT63T8T2DBlap9sJjwrIaiUllj2+lj2J9Crq18ow2reVFoPAQQbfwUpEwFkE+FZCUGltMSy18eyL0Cfrl4h/yTkZfGHgAlEwA8pE3oGDxDFd4ilwEoIKqUllr0+ll0AjzhGzxF98ZK4DCSIlDNOmEBj6bPohpUQVEpLLHt9LHsA3OEkeWrPmjy9HC4DJxARDVQmuhZKX3S3WAxWQlApLbHs9bHsV+gKyD3Bkqe9ahk4BwnkdOdDeMR5BzxiVb1Ux0oIKqUllr0+p5UdLrD3zJtaOCGecyafE13RKgNDkBDP0CqDEpgmd5IHEx7Rsg4CrISgUlpi2esztOxo1+wd8yTHdj7Btp8zHl9i1Y1pMjAEieXOCqYiLxk8g5UQVEpLLHt9hpQdbbmXM8xj1VUwGahggteEVYY1SbqNlJUQVEpLLHt9RpV97TPiSmDXf3WPWAYugYR7CJ9U+ayEoFJaYtnrU/KbYrwr02xBquzsriNeMziRlzT0lYHPQAYUHo1BFqAGSdfpWQlBpbTEstfnKL/pnmRf3XWfIwPXgMxS7sBJwd34CoT5p4D4/k0fSZUdZZPtuxROametVJWBa0GmvGxQeskgxLJXIMw/BcT3b/rI1rLTs6LVqTIwBRSA3fqaZ3nLXoEw/xQQ37/pI1t247PP5nNkYA4oDG+eSb1bTpFU+ayEoFJaYtnrY9mXQT2knfyeIQNLQOEoPc/0ud17n9krEOafAuL7N30kVfbUh8BM0BueNKs/FEYG1gIF5pie4q+9XJf8IgZWQlApLcl+yCbiWnbNqLKnXLXiZTQK3vSOURnYCuwMKucCuzhzeFA4wnLZpAqfg7iWXTOk7BNs19f2rdo8XSgei69FBh4JVkJQKS2x7PU5iuzZ5dwLMvBIoBIsuyDMPwXEt+yPWPatQSVYdkGYfwqIb9kfsexbg0qw7IIw/xQQ37I/Ytm3BpVg2QVh/ikgvmV/xLJvDSrBsgvC/FNAfMv+iGXfGlSCZReE+aeA+Jb9Ecu+NagEyy4I808B8S37I5Z9a1AJll0Q5p8C4lv2Ryz71qASLLsgzD8FxLfsj1j2rUEl9GyYXhtfH8veCRl4JFAJbpiCMP8UEN+/6SOWfWtQCW9BpbSkpGF+CdJqyarniC+B+K9Bei3JfmMQ4lr2BGTgkWAlBJXSkqIKF+m1IntugSB+z980++EMiNvzAGrZtwaV8D2olJYUPTUE8Xt1j0sPSj3PmEW3eCL+Z5BeK6o9MWYrZOCRQCV0O7qHeaeCNH6EaTai+CknSONXkGYLioYbBGl0GXKE+R4RGXg0UBk9zpjJT9EJQRpfgzRb8KnyTgXp9JgL+aHyTgFpfAvSbEHya8n2iAw8GqiMl6ByWlClG4d0Wh+YigUiSKd1V549hypPaUE6rX/Tw3fhiQw8IqiQlmO3ogmvOUirpUT8Dao95ghptZSo2oQX0mr5m1ar+62RgUcEldKywqs+6RPpvQfp16LqGQjpteoiVz0oEaTXYuzO3sdXld8RkYFHBRXT4pJRlW7xHKTJScXaE2DZq/uegXRr/6bc7+qPSSZIt3ZPZIju+4QMPDKooJoTS80mZpA2z5q1hG86gcT0g/xKyF5EEwNp8yBao9fEemlWzq2QgUcHFVWjS9d8EQXyqCF8kzN6CPIpFZ772eVMiXxKeiMcYjTpeWyNDBwBVBgX2+SIxMru1n1DXjwb5XQ/Wc6mLxUIQX686pHzm3L/uo59mR9IOUDx9zz8KrlnyMBRQOVRJC5kYUWqCp7DbTbruiFvTjCukX7rcvI35ZlzjfTsUm867kX+lJ5tgGX5AFPZWH7+3uwFdj1oboUMHBFUKLvMrHQ21Dk8W+2m24aysHGyTKqcu5oZRnl4gFK/KXtV3d50YtYhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMeMhA40x4yEDjTHjIQONMaPx+4//AV7STrgkLsp1AAAAAElFTkSuQmCC'
